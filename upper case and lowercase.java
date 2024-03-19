a2fd11b6ed14193444365bf59416e9cb672d1045
Anandu Suresh <149881625+Anandu0603@users.noreply.github.com>
2024-03-19 15:43:50 +0530

    Create upper case and lowercase.java

---
+++ upper case and lowercase.java
@@ -0,0 +1,16 @@
+public class StartString {
+        public static void main(String[]args){
+            String s1="Good Morning";
+            System.out.println(s1);
+            System.out.println("to upper case:"+s1.toUpperCase());
+            System.out.println("to upper case:"+s1.toLowerCase());
+        }
+    }
+
+                                            output
+                                              
+                                            Good Morning
+                                            to upper case:GOOD MORNING
+                                            to upper case:good morning
+                                            
+                                            Process finished with exit code 0
