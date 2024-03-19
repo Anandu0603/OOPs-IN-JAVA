86bee22060f49498dd7eb885901e1db94793945d
Anandu Suresh <149881625+Anandu0603@users.noreply.github.com>
2024-03-19 14:24:43 +0530

    Create replacestring.java

---
+++ replacestring.java
@@ -0,0 +1,17 @@
+class replacestring
+{
+    public static void main(String[] args)
+    {
+        String s1="Today is monday";
+        System.out.println("String is: "+s1);
+        String s2=s1.replace("is","=");
+        System.out.println(s2);
+
+    }
+}
+                                                      output
+                                                    
+                                                    String is: Today is monday
+                                                    Today = monday
+                                                    
+                                                    Process finished with exit code 0
